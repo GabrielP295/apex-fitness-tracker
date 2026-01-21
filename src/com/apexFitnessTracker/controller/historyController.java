d86338ce1c957db5d65e1055691fdac87caac6b5
GabrielP295 <gabyboyxp@gmail.com>
2026-01-21 13:56:44 -0600

    Added FXML id fields to the history controller

--- src/com/apexFitnessTracker/controller/historyController.java
+++ src/com/apexFitnessTracker/controller/historyController.java
@@ -6,6 +6,10 @@ import com.ApexFitnessTracker.util.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.VBox;
 
 public class HistoryController {
 
@@ -16,6 +20,19 @@ public class HistoryController {
 	@FXML private Button libraryBtn;
 	@FXML private Button logWorkoutBtn;
 	@FXML private Button profileBtn;
+	
+	// ----- Workout Container UI -----
+	@FXML private Label workoutNameLbl;
+	@FXML private Label dateTimeLbl;
+	@FXML private Label lengthLbl;
+	@FXML private Label volumeLbl;
+	@FXML private Label prLbl;
+	@FXML private TitledPane exerciseTitledPane;
+	
+	// ----- Other UI -----
+	@FXML private DatePicker datePickerFld;
+	@FXML private VBox workoutContainer;
+	
 
 	@FXML
 	void onBodyBtn(ActionEvent event) {
---
+++ src/com/apexFitnessTracker/repository/WorkoutRepository.java
@@ -0,0 +1,5 @@
+package com.ApexFitnessTracker.repository;
+
+public interface WorkoutRepository {
+
+}
